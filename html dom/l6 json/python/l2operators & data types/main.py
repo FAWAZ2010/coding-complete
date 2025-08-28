787e34f95c7fbd7bfb98a94408301e06fd1415e8
FAWAZ2010 <maisurf27@yahoo.com>
2025-08-28 17:56:12 +0000

    solo sikoa bi

---
+++ html dom/l6 json/python/l2operators & data types/main.py
@@ -0,0 +1,19 @@
+name = "Fawaz"
+age = 14
+is_student = True
+weight = 65.5
+print("Name:",name)
+print("data type of Name is:",type(name))
+print("Age:",age)
+print("data type of age is:",type(age))
+print("Is student:",is_student)
+print("Data type of Is_ student is:",type (is_student))
+print("weight:",weight)
+print("data type of weight is ",type(weight))
+print("/n after Type Casting....")
+age = str(age)
+print("age")
+print("data type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is",type(weight))
---
+++ html dom/l6 json/python/l2operators & data types/main2.py
@@ -0,0 +1,22 @@
+num1 = 45
+num2 = 10
+print("Number 1 ",num1)
+print("Number 2",num2)
+print("Adddition:", num1+num2)
+print("Subtraction",num1-num2)
+print("product",num1*num2)
+print("Number 2",num2)
+print("Division",num1/num2)
+print("Floor Division",num1//num2)
+print("Modulus Operation",num1%num2)
+print("square:",num2**2)
+print("square Root:",num1**0.5)
+
+print("Equal ? ",num1==num2)
+print("Number 1 Greater ?">num2)
+print("Number 2 greater ?",num1<num2)
+print("Number 1 Greater or equal ?",num1>=num2)
+print("Number 2 Greater or equal ?",num1<=num2)
+print("Not Equal ?",num1!=num2)
+result = num1/2+num2**2+10
+print("Result is:",result)
