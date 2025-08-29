8f406f925c73ae4b482c0427d9428ecd213ad14d
FAWAZ2010 <maisurf27@yahoo.com>
2025-08-29 17:17:15 +0000

    sada

---
+++ html dom/l6 json/python/constatement/main.py
@@ -0,0 +1,7 @@
+number = int(input("enter Number to check"))
+print("Number to be checked :" ,number)
+if number % 2 == 0:
+    print("This is Even Number")
+else:
+    print("This is an odd number")
+    30
