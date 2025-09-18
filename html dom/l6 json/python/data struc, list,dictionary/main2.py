7737b7126935bfe55e1732146b757c3a5c45bc5a
FAWAZ2010 <maisurf27@yahoo.com>
2025-09-18 17:48:14 +0000

    abnm,

---
+++ html dom/l6 json/python/data struc, list,dictionary/main2.py
@@ -0,0 +1,15 @@
+my_dict = {}
+my_dict = {1: "apple", 2: "banana"}
+my_dict = {"name":"Sam", 2: "banana"}
+my_dict["name":"Fawaz", "age": 25, ]
+print(my_dict["name"])
+print(my_dict.get("age"))
+my_dict["age"] = 26
+print(my_dict)
+my_dict["Address"] = "Abu Dhabi"
+print(my_dict)
+my_dict.pop("age")
+print(my_dict)
+print("Address :", my_dict("address"))
+my_dict.clear()
+print(my_dict)
