5aa81083f19e136e6cfb272a62546da7f02f8bd9
FAWAZ2010 <maisurf27@yahoo.com>
2025-09-19 17:55:15 +0000

    sasasassfbnm,.

---
+++ html dom/l6 json/python/data stuc 2 set tuplein py/main3.py
@@ -0,0 +1,8 @@
+setx = {"red", "yellow"}
+sety = {"red", "blue"}
+print("Original set elements :")
+print(setx)
+print(sety)
+print("\nisection of two sets :")
+setz = setx.intersection(sety)
+print(setz)
---
+++ html dom/l6 json/python/data stuc 2 set tuplein py/main4.py
@@ -0,0 +1,8 @@
+setx = {"red", "yellow"}
+sety = {"red", "blue"}
+print("Original set elements :")
+print(setx)
+print(sety)
+print("\nunionof two sets :")
+setz = setx.union(sety)
+print(setz)
